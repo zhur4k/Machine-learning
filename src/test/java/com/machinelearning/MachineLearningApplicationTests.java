c64bae28a121f334dc2a137efc7e5dbe12bd84ff
zhur4k <kostya.zhurovich12052@gmail.com>
2024-10-22 09:28:12 +0300

    Initial project.

---
+++ src/test/java/com/machinelearning/MachineLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machinelearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
